15bd366502321a0b0ef331501b63cff4cf70d781
Przemyslaw <pgaffka@st.swps.edu.pl>
2025-02-05 23:57:23 +0100

    Autoryzacja

--- zaliczenie/myapp/urls.py
+++ zaliczenie/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'kluby', KlubPilkarskiViewSet)
@@ -11,4 +12,8 @@ router.register(r'statystyki', StatystykiViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+]
+
+urlpatterns += [
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
